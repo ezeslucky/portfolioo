7429f6d891ab06eace172aa29d384d3c29c0d4a4
Lucky <lucky123@gmail.com>
2024-12-15 20:10:34 +0530

    portfolio

---
+++ src/assets/images/index.js
@@ -0,0 +1,16 @@
+import hero from './hero.jpg'
+// import meta from './meta.png'
+// import shopify from './shopify.png'
+// import starbucks from './starbucks.png'
+// import tesla from './tesla.png'
+import logo from './logo.svg'
+import nullclass from './nullclass.png'
+import grappltech from './grappltech.png'
+
+export {
+    hero,
+    
+    logo,
+    nullclass,
+    grappltech
+}
